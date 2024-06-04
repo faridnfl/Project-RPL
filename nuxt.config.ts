9370c08a2124d4e3af8b9dcad9285bcbc70d64b9
faridnfl <faridnaufal34@gmail.com>
2024-06-04 20:43:56 +0800

    logo and name change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,5 +14,18 @@ export default defineNuxtConfig({
         wght: [500],
       }
     }
+  },
+  app: {
+    head: {
+      title: 'Inbound and Outbound Form Hasanuddin University',
+      htmlAttrs: {
+        lang: 'en'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+      link: [{ rel: 'icon', type: 'image/png', href: '/logo-unhas.png' }]
+    },
   }
 })
